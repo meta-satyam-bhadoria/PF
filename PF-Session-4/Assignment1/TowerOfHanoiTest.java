073c44f02b76fa0bf453421e9bbce95de53d37d8
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-01-30 18:08:59 +0530

    This is my PF-Session-4 Assignment

--- PF-Session-4/Assignment1/TowerOfHanoiTest.java
+++ PF-Session-4/Assignment1/TowerOfHanoiTest.java
@@ -1,3 +1,6 @@
+/*
+ * This test case file is to test TowerOfHanoi class working.
+ */
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
@@ -6,10 +9,15 @@ import java.util.List;
 
 import org.junit.Test;
 
-
+/**
+ * class to test QuickSort class implementation
+ * @author satyam bhadoria
+ *
+ */
 public class TowerOfHanoiTest {
-	TowerOfHanoi towerOfHanoi = new TowerOfHanoi();
+	protected TowerOfHanoi towerOfHanoi = new TowerOfHanoi();		//creating object
 
+	/*positive test case*/
 	@Test
 	public void testTowerOfHanoi() {
 		int n = 3;
@@ -20,28 +28,27 @@ public class TowerOfHanoiTest {
 				"Move disk 1 from rod B to rod A" ,
 				"Move disk 2 from rod B to rod C" ,
 				"Move disk 1 from rod A to rod C" );
-		List<String> actual = new ArrayList<String>();
 		try {
-			assertEquals(expected, towerOfHanoi.towerOfHanoi(actual, n, 'A', 'C', 'B'));
+			assertEquals(expected, towerOfHanoi.towerOfHanoi(n, 'A', 'C', 'B'));
 		} catch (DiskError e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/*negative test case*/
 	@Test(expected = DiskError.class)
 	public void testTowerOfHanoi1() throws DiskError {
 		int n = 0;
 		List<String> expected=Arrays.asList("");
-		List<String> actual = new ArrayList<String>();
-		assertEquals(expected, towerOfHanoi.towerOfHanoi(actual, n, 'A', 'C', 'B'));
+		assertEquals(expected, towerOfHanoi.towerOfHanoi(n, 'A', 'C', 'B'));
 	}
 	
+	/*negative test case*/
 	@Test(expected = DiskError.class)
 	public void testTowerOfHanoi2() throws DiskError {
 		int n = -2;
 		List<String> expected=Arrays.asList("");
-		List<String> actual = new ArrayList<String>();
-		assertEquals(expected, towerOfHanoi.towerOfHanoi(actual, n, 'A', 'C', 'B'));
+		assertEquals(expected, towerOfHanoi.towerOfHanoi(n, 'A', 'C', 'B'));
 	}
 
 }
