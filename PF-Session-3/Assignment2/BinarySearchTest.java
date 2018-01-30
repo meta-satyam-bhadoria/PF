72057005966114967b3943d2248dfc127feaad6f
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-01-30 17:32:24 +0530

    This is my PF-Session-3 Assignment

--- PF-Session-3/Assignment2/BinarySearchTest.java
+++ PF-Session-3/Assignment2/BinarySearchTest.java
@@ -1,39 +1,50 @@
+/*
+ * This test case file is to test BinarySearch class working.
+ */
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
-
+/**
+ * class to test BinarySearch class implementation
+ * @author satyam bhadoria
+ *
+ */
 public class BinarySearchTest {
-	BinarySearch search;
+	protected BinarySearch search;		//create object
 	
+	/*initialize object*/
 	@Before
 	public void testThis(){
 		search = new BinarySearch();
 	}
 
+	/*positive test case*/
 	@Test
 	public void test() {
 		int[] inputArr = new int[] {2,5,8,9,10,55,77};
 		int element = 88;
 		boolean output = false;
-		assertEquals(output, search.binarySearch(inputArr, element, 0, inputArr.length));
+		assertEquals(output, search.binarySearch(inputArr, element));
 	}
 	
+	/*positive test case*/
 	@Test
 	public void test1() {
 		int[] inputArr = new int[] {2,5,8,9,10,55,77,88};
 		int element = 77;
 		boolean output = true;
-		assertEquals(output, search.binarySearch(inputArr, element, 0, inputArr.length));
+		assertEquals(output, search.binarySearch(inputArr, element));
 	}
 	
-	@Test
+	/*negative test case*/
+	@Test(expected = ArithmeticException.class)
 	public void test2() {
 		int[] inputArr = new int[] {};
 		int element = 77;
 		boolean output = false;
-		assertEquals(output, search.binarySearch(inputArr, element, 0, inputArr.length));
+		assertEquals(output, search.binarySearch(inputArr, element));
 	}
 
 }
