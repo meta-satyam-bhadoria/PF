efd0ea221695a95bbb2be57547c866e0ebeca30d
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-01-30 17:04:52 +0530

    This is my PF-Session-3 Assignment

--- PF-Session-3/Assignment1/RemTest.java
+++ PF-Session-3/Assignment1/RemTest.java
@@ -1,3 +1,6 @@
+/*
+ * This test case file is to test Rem class working.
+ */
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
@@ -8,28 +11,39 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-
+/**
+ * class to test Rem class implementation
+ * @author satyam bhadoria
+ *
+ */
 @RunWith(Parameterized.class)
 public class RemTest {
 
-	Rem rem = new Rem();
+	protected Rem rem = new Rem();
 		 
+	/*inputs and expected output as parameters*/
 	@Parameters
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] {
-				{ 1, 100, 3 } , { 0, 3, 0 } , { 0, 0, 32 } , { 0, 3, 1} , { 1, 45, 4}, {4, 49, 9}
+				{ 1, 100, 3 } , { 0, 3, 0 } , { 0, 0, 32 } , { 0, 3, 1} , {0,3,-2}, {0, -9, 2}
 		});
 	}
+	
+	/*variables for inputs and expected output*/
 	private int fInput1;
 	private int fInput2;
 	private int fExpected;
 	
+	/**
+	 * method for initializing variables declared above
+	 */
 	public RemTest(int expected, int number1, int number2) {
 		fInput1= number1;
 		fInput2 = number2;
 		fExpected= expected;
 	}
 	
+	/*test cases including negative(2,5 and 6 case) and positive(rest case)*/
 	@Test
 	public void test() {
 		try {
