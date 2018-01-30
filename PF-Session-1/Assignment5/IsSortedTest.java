b55271c769190ab845f0e042b870b617d1f7d0f4
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-01-30 15:54:30 +0530

    changes made

--- PF-Session-1/Assignment5/IsSortedTest.java
+++ PF-Session-1/Assignment5/IsSortedTest.java
@@ -1,58 +1,80 @@
+/*
+ * test case file to test IsSorted class working
+ */
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
-
+/**
+ * class to test IsSorted class implementation
+ * @author satyam bhadoria
+ *
+ */
 public class IsSortedTest {
 	
-	IsSorted sorted;
+	protected IsSorted sorted;		//creating object
 
+	/*initializing object*/
 	@Before
 	public void testThis(){
 		sorted = new IsSorted();
 	}
-
+	
+	/*positive test case 1*/
 	@Test
 	public void test() {
 		int[] input = new int[] {1,2,3,4};
 		int output = 1;
-		assertEquals(output, sorted.isSorted(input));
+		assertEquals (output, sorted.isSorted (input));
 	}
 	
+	/*positive test case 2*/
 	@Test
 	public void test1() {
 		int[] input = new int[] {1,2,4,5,6,6,7,8,9};
 		int output = 1;
-		assertEquals(output, sorted.isSorted(input));
+		assertEquals (output, sorted.isSorted (input));
 	}
 	
+	/*positive test case 3*/
 	@Test
 	public void test2() {
 		int[] input = new int[] {9,5,4,4,3,1};
 		int output = 2;
-		assertEquals(output, sorted.isSorted(input));
+		assertEquals (output, sorted.isSorted (input));
 	}
 	
+	/*positive test case 4*/
 	@Test
 	public void test3() {
 		int[] input = new int[] {1,2,5,4};
 		int output = 0;
-		assertEquals(output, sorted.isSorted(input));
+		assertEquals (output, sorted.isSorted (input));
 	}
 	
+	/*positive test case 5*/
 	@Test
 	public void test4() {
 		int[] input = new int[] {-1,-2,-3,-4,-5,-6};
 		int output = 2;
-		assertEquals(output, sorted.isSorted(input));
+		assertEquals (output, sorted.isSorted (input));
 	}
 	
+	/*positive test case 6*/
 	@Test
 	public void test5() {
 		int[] input = new int[] {1,1,1,1,1,1,1,1,1};
 		int output = 1;
-		assertEquals(output, sorted.isSorted(input));
+		assertEquals (output, sorted.isSorted (input));
+	}
+	
+	/*negative test case 7*/
+	@Test(expected = ArithmeticException.class)
+	public void test6() {
+		int[] input = new int[] {};
+		int output = 1;
+		assertEquals (output, sorted.isSorted (input));
 	}
 
 }
