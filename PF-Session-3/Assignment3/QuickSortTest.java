44297c46605439f1fc93ed01fc6053cf4c4ba04a
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-01-30 17:41:32 +0530

    This is my PF-Session-3 Assignment

--- PF-Session-3/Assignment3/QuickSortTest.java
+++ PF-Session-3/Assignment3/QuickSortTest.java
@@ -1,9 +1,19 @@
+/*
+ * This test case file is to test QuickSort class working.
+ */
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * class to test QuickSort class implementation
+ * @author satyam bhadoria
+ *
+ */
 public class QuickSortTest {
-	QuickSort sort = new QuickSort();
+	protected QuickSort sort = new QuickSort();		//creating object
+	
+	/*positive test case*/
 	@Test
 	public void test() {
 		int inputArray[] = new int[] {1,5,2,8,3,9,23,67,13};
@@ -13,21 +23,33 @@ public class QuickSortTest {
 		assertArrayEquals(expected, sort.quickSort(inputArray, startIndex, endIndex));
 	}
 	
+	/*positive test case*/
 	@Test
 	public void test1() {
-		int inputArray[] = new int[] {1,5,2,4,9,2,1,7,65,14};
+		int inputArray[] = new int[] {1,5,2,4,-9,2,-1,-7,65,14};
 		int startIndex = 0;
 		int endIndex = 9;
-		int expected[] = new int[] {1,1,2,2,4,5,7,9,14,65};
+		int expected[] = new int[] {-9,-7,-1,1,2,2,4,5,14,65};
 		assertArrayEquals(expected, sort.quickSort(inputArray, startIndex, endIndex));
 	}
 	
+	/*positive test case*/
 	@Test
 	public void test2() {
 		int inputArray[] = new int[] {1,1,1,1,1};
 		int startIndex = 0;
-		int endIndex = 5;
+		int endIndex = 4;
 		int expected[] = new int[] {1,1,1,1,1};
+		assertArrayEquals(expected, sort.quickSort(inputArray, startIndex, endIndex));
+	}
+	
+	/*negative test case*/
+	@Test(expected = ArithmeticException.class)
+	public void test3() {
+		int inputArray[] = new int[] {};
+		int startIndex = 0;
+		int endIndex = 0;
+		int expected[] = new int[] {};
 		assertArrayEquals(expected, sort.quickSort(inputArray, startIndex, endIndex));
 	}
 
