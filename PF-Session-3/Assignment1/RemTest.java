306c498e299db816ad67d32837069d0893842de6
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-02-01 11:15:02 +0530

    This is my PF-Session-3 Assignment

--- PF-Session-3/Assignment1/RemTest.java
+++ PF-Session-3/Assignment1/RemTest.java
@@ -2,55 +2,52 @@
  * This test case file is to test Rem class working.
  */
 import static org.junit.Assert.*;
-
-import java.util.Arrays;
-import java.util.Collection;
-
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 /**
  * class to test Rem class implementation
  * @author satyam bhadoria
  *
  */
-@RunWith(Parameterized.class)
 public class RemTest {
-
-	protected Rem rem = new Rem();
-		 
-	/*inputs and expected output as parameters*/
-	@Parameters
-	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] {
-				{ 1, 100, 3 } , { 0, 3, 0 } , { 0, 0, 32 } , { 0, 3, 1} , {0,3,-2}, {0, -9, 2}
-		});
+	public Rem rem;			//creating object
+	
+	/*initialize object*/
+	@Before
+	public void init () {
+		rem = new Rem();
 	}
 	
-	/*variables for inputs and expected output*/
-	private int fInput1;
-	private int fInput2;
-	private int fExpected;
-	
-	/**
-	 * method for initializing variables declared above
-	 */
-	public RemTest(int expected, int number1, int number2) {
-		fInput1= number1;
-		fInput2 = number2;
-		fExpected= expected;
-	}
-	
-	/*test cases including negative(2,5 and 6 case) and positive(rest case)*/
+	/*successful test case for finding remainder*/
 	@Test
-	public void test() {
-		try {
-			assertEquals(fExpected, rem.rem(fInput1, fInput2));
-		}
-		catch (ArithmeticException e) {
-			e.printStackTrace();
-		}
+	public void testRem() {
+		int expected = 15;
+		int actual = rem.rem(15, 20);
+		assertEquals(expected, actual);
+	}
+	
+	/*failure test case for finding remainder when first number is negaitve*/
+	@Test(expected = ArithmeticException.class)
+	public void testRem1() {
+		int expected = 0;
+		int actual = rem.rem(-3, 5);
+		assertEquals(expected, actual);
+	}
+	
+	/*failure test case for finding remainder when second number is 0*/
+	@Test(expected = ArithmeticException.class)
+	public void testRem2() {
+		int expected = 0;
+		int actual = rem.rem(3, 0);
+		assertEquals(expected, actual);
+	}
+
+	/*failure test case for finding remainder when second number is negative*/
+	@Test(expected = ArithmeticException.class)
+	public void testRem3() {
+		int expected = 0;
+		int actual = rem.rem(3, -5);
+		assertEquals(expected, actual);
 	}
 }
