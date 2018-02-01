9648a765067152af25387282f02c8487e6342b67
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-02-01 12:05:39 +0530

    This is my PF-Session-4 Assignment

--- PF-Session-4/Assignment2/GeneratePermutations.java
+++ PF-Session-4/Assignment2/GeneratePermutations.java
@@ -17,10 +17,10 @@ public class GeneratePermutations {
 	/**
 	 * This method call another method to generate
 	 * permutations.
-	 * @param string - to which combination to be generated.
+	 * @param string - to which combinations to be generated.
 	 * @return set - set of all possible combination
 	 */
-	protected List<String> generatePermutations (String string) {
+	public List<String> generatePermutations (String string) {
 		List<String> set = new ArrayList<String>();			//store set of all possible permutations.
 		
 		/*throw exception if no string is passed or string size 0 is passed*/
@@ -34,11 +34,11 @@ public class GeneratePermutations {
 	/**
 	 * This method actually implements the algorithm
 	 * @param set - 
-	 * @param string - combination to be generated
+	 * @param string - combinations to be generated
 	 * @param stringIndex - from which to find permutations
 	 * @return set - set of all permutations
 	 */
-	private List<String> generate (List<String> set, String string, int stringIndex) {
+	public List<String> generate (List<String> set, String string, int stringIndex) {
 		char[] str = string.toCharArray();			//store string in array form
 		
 		/*if operation done on final index return set */
@@ -50,13 +50,14 @@ public class GeneratePermutations {
 		/*perform operation from string index to final index to find permutations*/
 		for(int i = stringIndex; i <= string.length(); i++){
 			string = new String(str, 0, string.length());
+			
 			generate(set, string, stringIndex + 1);			//repeat process recursive on sub string
 			
 			/*swap two adjacent letters to create new string*/
 			if(i < string.length()){
-				char temp = str[i];
+				char temporaryChar = str[i];
 				str[i] = str[stringIndex - 1];
-				str[stringIndex - 1] = temp;
+				str[stringIndex - 1] = temporaryChar;
 			}
 		}
 		return set;
