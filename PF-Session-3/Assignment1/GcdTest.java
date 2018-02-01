9b32befd4e98b2b19ba64ae3dbcf4cd99f6ff09d
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-02-01 11:14:20 +0530

    This is my PF-Session-3 Assignment

--- PF-Session-3/Assignment1/GcdTest.java
+++ PF-Session-3/Assignment1/GcdTest.java
@@ -2,54 +2,44 @@
  * This test case file is to test Gcd class working.
  */
 import static org.junit.Assert.*;
-
-import java.util.Arrays;
-import java.util.Collection;
-
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 /**
  * class to test Gcd class implementation
  * @author satyam bhadoria
  *
  */
-@RunWith(Parameterized.class)
 public class GcdTest {
-	protected Gcd gcd = new Gcd();				//creating object
-		 
-	/*inputs and expected output as parameters*/
-	@Parameters
-	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] {
-				{ 1, 100, 3 } , { 1, 3, -100 } , { 5, 50, 45 } , { 3, 3, 0} , { 1, -5, -9}, {0, 0, 0}
-		});
+	public Gcd gcd;			//creating object
+	
+	/*initialize object*/
+	@Before
+	public void init () {
+		gcd = new Gcd();
 	}
 	
-	/*variables for inputs and expected output*/
-	private int fInput1;
-	private int fInput2;
-	private int fExpected;
-	
-	/**
-	 * method for initializing variables declared above
-	 */
-	public GcdTest(int expected, int number1, int number2) {
-		fInput1= number1;
-		fInput2 = number2;
-		fExpected= expected;
-	}
-	
-	/*test cases including negative(2 and 5 case) and positive(rest case)*/
+	/*successful test case for finding gcd*/
 	@Test
-	public void test() {
-		try{
-			assertEquals(fExpected, gcd.gcd(fInput1, fInput2));
-		}
-		catch (ArithmeticException e) {
-			e.printStackTrace();
-		}
+	public void testGcd() {
+		int expected = 5;
+		int actual = gcd.gcd(15, 20);
+		assertEquals(expected, actual);
+	}
+	
+	/*failure test case for finding gcd when first number is neagtive*/
+	@Test(expected = ArithmeticException.class)
+	public void testGcd1() {
+		int expected = 0;
+		int actual = gcd.gcd(-3, 5);
+		assertEquals(expected, actual);
+	}
+
+	/*failure test case for finding gcd when second number is neagtive*/
+	@Test(expected = ArithmeticException.class)
+	public void testGcd2() {
+		int expected = 0;
+		int actual = gcd.gcd(3, -5);
+		assertEquals(expected, actual);
 	}
 }
