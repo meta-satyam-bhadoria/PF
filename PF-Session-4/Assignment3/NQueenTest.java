74844d913b3843b19305d93e17e25896af5c1fd9
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-02-01 12:28:15 +0530

    This is my PF-Session-4 Assignment

--- PF-Session-4/Assignment3/NQueenTest.java
+++ PF-Session-4/Assignment3/NQueenTest.java
@@ -12,11 +12,11 @@ import org.junit.Test;
  */
 public class NQueenTest {
 
-	protected NQueen nQueen = new NQueen();
+	public NQueen nQueen = new NQueen();
 	
-	/*positive test case*/
+	/*successful test case for nQueen*/
 	@Test
-	public void test() {
+	public void testNQueen() {
 		int dimension = 4;
 		int[][] board = new int[dimension][dimension];
 		assertEquals(true, nQueen.nQueen(board, 0, dimension));
@@ -30,20 +30,36 @@ public class NQueenTest {
 		}
 	}
 	
-	/*positive test case*/
+	/*successful test case for nQueen*/
 	@Test
-	public void test1() {
+	public void testNQueen1() {
 		int dimension = 3;
 		int[][] board = new int[dimension][dimension];
 		assertEquals(false, nQueen.nQueen(board, 0, dimension));
 	}
 	
-	/*negative test case*/
+	/*failure test case for nQueen when dimension of board is 0*/
 	@Test(expected = ArithmeticException.class)
-	public void test2() {
+	public void testNQueen2() {
 		int dimension = 0;
 		int[][] board = new int[dimension][dimension];
 		assertEquals(false, nQueen.nQueen(board, 0, dimension));
+	}
+	
+	/*failure test case for nQueen when starting row is negative*/
+	@Test(expected = ArithmeticException.class)
+	public void testNQueen3() {
+		int dimension = 4;
+		int[][] board = new int[dimension][dimension];
+		assertEquals(false, nQueen.nQueen(board, -1, dimension));
+	}
+	
+	/*failure test case for nQueen when dimension is greater board size*/
+	@Test(expected = ArrayIndexOutOfBoundsException.class)
+	public void testNQueen4() {
+		int dimension = 4;
+		int[][] board = new int[dimension][dimension];
+		assertEquals(false, nQueen.nQueen(board, 0, 5));
 	}
 
 }
