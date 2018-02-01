23da8acf741dfcdaeffe8ae490cb0dde1982f99f
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-02-01 11:16:45 +0530

    This is my PF-Session-3 Assignment

--- PF-Session-3/Assignment1/Gcd.java
+++ PF-Session-3/Assignment1/Gcd.java
@@ -19,7 +19,7 @@ public class Gcd {
 	 * @param num1 - first number
 	 * @param num2 - second number
 	 */
-	protected int gcd (int num1, int num2) {
+	public int gcd (int num1, int num2) {
 		
 		if (num1 < 0 || num2 < 0) {
 			throw new ArithmeticException ("Numbers should be positive");
