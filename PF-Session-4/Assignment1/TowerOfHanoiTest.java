a88c4267c8e436ae8a6fd38da7c289bcf0cb0aac
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-02-01 11:51:40 +0530

    This is my PF-Session-4 Assignment

--- PF-Session-4/Assignment1/TowerOfHanoiTest.java
+++ PF-Session-4/Assignment1/TowerOfHanoiTest.java
@@ -15,37 +15,37 @@ import org.junit.Test;
  *
  */
 public class TowerOfHanoiTest {
-	protected TowerOfHanoi towerOfHanoi = new TowerOfHanoi();		//creating object
+	public TowerOfHanoi towerOfHanoi = new TowerOfHanoi();		//creating object
 
-	/*positive test case*/
+	/*successful test case for tower of hanoi*/
 	@Test
 	public void testTowerOfHanoi() {
 		int n = 3;
 		List<String> expected=Arrays.asList("Move disk 1 from rod A to rod C",
 				"Move disk 2 from rod A to rod B",
-				"Move disk 1 from rod C to rod B" ,
-				"Move disk 3 from rod A to rod C" ,
-				"Move disk 1 from rod B to rod A" ,
-				"Move disk 2 from rod B to rod C" ,
-				"Move disk 1 from rod A to rod C" );
+				"Move disk 1 from rod C to rod B",
+				"Move disk 3 from rod A to rod C",
+				"Move disk 1 from rod B to rod A",
+				"Move disk 2 from rod B to rod C",
+				"Move disk 1 from rod A to rod C");
 		try {
 			assertEquals(expected, towerOfHanoi.towerOfHanoi(n, 'A', 'C', 'B'));
-		} catch (DiskError e) {
+		} catch (ArithmeticException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	/*negative test case*/
-	@Test(expected = DiskError.class)
-	public void testTowerOfHanoi1() throws DiskError {
+	/*failure test case for tower of hanoi when number of disk is 0*/
+	@Test(expected = ArithmeticException.class)
+	public void testTowerOfHanoi1() throws ArithmeticException {
 		int n = 0;
 		List<String> expected=Arrays.asList("");
 		assertEquals(expected, towerOfHanoi.towerOfHanoi(n, 'A', 'C', 'B'));
 	}
 	
-	/*negative test case*/
-	@Test(expected = DiskError.class)
-	public void testTowerOfHanoi2() throws DiskError {
+	/*failure test case for tower of hanoi when number of disk is in negative*/
+	@Test(expected = ArithmeticException.class)
+	public void testTowerOfHanoi2() throws ArithmeticException {
 		int n = -2;
 		List<String> expected=Arrays.asList("");
 		assertEquals(expected, towerOfHanoi.towerOfHanoi(n, 'A', 'C', 'B'));
