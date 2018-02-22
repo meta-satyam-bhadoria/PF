64602bb3928063181f6b13344e1d44f558abfb9d
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-02-22 14:49:44 +0530

    This is JavaScript-Session-1 Assignment.

---
+++ JavaScript Session-1/Assignment3/script.js
@@ -0,0 +1,34 @@
+displayThumbnail ();
+
+var btn = document.querySelector ('.dark');
+btn.onclick = function () {
+	if ( btn.getAttribute ('class') == 'dark'){
+		btn.setAttribute ('class', 'light');
+		btn.textContent = 'Lighten';
+		document.querySelector ('.overlay').style.background = 'rgba(0,0,0,.5)';
+	} else{
+		btn.setAttribute ('class', 'dark');
+		btn.textContent = 'Darken';
+		document.querySelector ('.overlay').style.background = 'rgba(0,0,0,0)';
+	}
+}
+
+/*looping for thumb-bar*/
+function displayThumbnail () {
+	var div = document.querySelector ('#thumb-bar');
+	for (var i = 1;i <= 5; i++) {
+		var image = document.createElement ("img");
+		image.setAttribute ('src', 'images/pic'+i+'.jpg');
+		div.append (image);
+		image.onclick = function (e) {
+			var source = e.target.getAttribute ('src');
+			showImage (source);
+		}
+	}
+}
+
+/*displayed image*/
+function showImage (source) {
+	var x = document.querySelector ('#displayed-img');
+	x.setAttribute ('src',source);
+}
