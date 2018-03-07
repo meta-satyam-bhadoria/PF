aca262bde8672cd437b030796fb1191a2fb41147
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-03-07 16:59:37 +0530

    This is DesignPattern-Session-2 Assignment

--- DesignPattern-Session-2/com/metacube/shoppingcart/main/Main.java
+++ DesignPattern-Session-2/com/metacube/shoppingcart/main/Main.java
@@ -7,19 +7,21 @@ public class Main {
 	public static void main(String[] args){
 		Scanner inputScanner  = new Scanner(System.in);
 		Input shop = new Input();
+		int choice;
 		do{
-			System.out.println("1. Show All Products");
-			System.out.println("2. Add Product");
-			System.out.println("3. Delete Product");
-			System.out.println("4. Update Product");
-			System.out.println("5. Show Cart Products");
+			System.out.println("1. Show all products");
+			System.out.println("2. Add product");
+			System.out.println("3. Delete product");
+			System.out.println("4. Update product");
+			
+			System.out.println("5. Add user");
+			System.out.println("6. Update user");
+			System.out.println("7. Show all user");
+			
+			System.out.println("8. Shop");
 			System.out.println("Enter ur choice");
-			int choice = inputScanner.nextInt();
-			if(choice <= 0 && choice < 5){
-				break;
-			}
-			shop.takeInput(choice);
-		}while(true);
+			choice = inputScanner.nextInt();
+		}while(shop.takeInput(choice));
 		inputScanner.close();
 	}
 }
