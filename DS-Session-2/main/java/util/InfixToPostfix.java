355b9f1d942961e50362cec8f238118882cdc060
meta-satyam-bhadoria <35562595+meta-satyam-bhadoria@users.noreply.github.com>
2018-03-16 16:52:55 +0530

    This is DataStructure-Session-2 Assignment.

---
+++ DS-Session-2/main/java/util/InfixToPostfix.java
@@ -0,0 +1,13 @@
+package main.java.util;
+
+public class InfixToPostfix {
+	private Stack<Character> stack;
+	
+	public InfixToPostfix() {
+		stack = new Stack<>();
+	}
+	
+	public void convertToPostfix() {
+		
+	}
+}
---
+++ DS-Session-2/main/java/util/Stack.java
@@ -0,0 +1,91 @@
+package main.java.util;
+
+/**
+ * This class implements the stack
+ * @author satyam bhadoria
+ *
+ * @param <T> - generic parameter
+ */
+public class Stack<T> {
+	private Node<T> top;
+	
+	/**
+	 * constructor
+	 */
+	public Stack() {
+		this.top = null;
+	}
+	
+	/**
+	 * method to push element on the top of stack
+	 * @param item - element to push
+	 */
+	public void push(T item) {
+		Node<T> newNode = new Node<>(item);
+		newNode.setNextLink(this.top);
+		this.top = newNode;
+	}
+	
+	/**
+	 * method to pop the top most element from the stack
+	 * @return top element
+	 */
+	public T pop() {
+		T removedElement ;
+		
+		if(this.top != null) {
+			removedElement = this.top.getData();
+			this.top = this.top.getNextLink();
+			
+		} else{
+			throw new IndexOutOfBoundsException("no element to remove");
+		}
+		return removedElement;
+	}
+	
+	/**
+	 * method which return the top element of stack
+	 * @return top element
+	 */
+	public T peek() {
+		
+		if(this.top == null) {
+			throw new IndexOutOfBoundsException("stack is empty");
+			
+		} else{ 
+			return this.top.getData();
+		}
+	}
+	
+	/**
+	 * method which returns the size of stack
+	 * @return
+	 */
+	private int size() {
+		int size = 0;
+		Node<T> currentNode = this.top;
+		
+		while(currentNode != null) {
+			size++;
+			currentNode = currentNode.getNextLink();
+		}
+		return size;
+	}
+	
+	/**
+	 * method which returns stack element as an array
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public T[] toArray() {
+		T[] array = (T[]) new Object[ this.size() ];
+		Node<T> currentNode = this.top;
+		int i = 0;
+		
+		while(currentNode != null) {
+			array[i++] = currentNode.getData();
+			currentNode = currentNode.getNextLink();
+		}
+		return array;
+	}
+}
